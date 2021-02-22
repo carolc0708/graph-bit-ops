533371ab177e44329fefb4f43aa678f1b73b1d71
carolc0708 <carolc830708@gmail.com>
2021-02-21 23:40:24 -0500

    Add bmv64

--- bsrbmv/benchmark-bsrbmv.cu
+++ bsrbmv/benchmark-bsrbmv.cu
@@ -91,6 +91,31 @@ void printbinmat(unsigned* binbsrval, const int nblocks, const int blocksize)
     }
 }
 
+__global__ void printpackvec (ullong* packvec, const int N)
+{
+    for(int i=0; i<N; i++) {
+        ullong j;
+        for(j = 1ULL << 63; j > 0; j = j / 2)
+            (packvec[i] & j) ? printf("1") : printf("0");
+    }
+    printf("\n");
+}
+
+__global__ void printpackmat (ullong* packbsrval,  const int nblocks, const int blocksize)
+{
+    for(int i=0; i<nblocks; i++) {
+        printf("[%d]\n", i);
+        for(int j=0; j<blocksize; j++) {
+            ullong k;
+            for(k = 1ULL << 63; k > 0; k = k / 2)
+                (packbsrval[i*blocksize+j] & k) ? printf("1") : printf("0");
+
+            printf("\n");
+        }
+        printf("\n");
+    }
+}
+
 void printind(int* indarr, const int N)
 {
     for(int i=0; i<N; i++) {
@@ -99,11 +124,8 @@ void printind(int* indarr, const int N)
     printf("\n");
 }
 
-int main(int argc, char* argv[])
+int main32(int argc, char* argv[])
 {
-
-//    bool trans_A = false;
-//    bool trans_B = false;
 
     cudaSetDevice(0);
     if (argc < 2)
@@ -186,7 +208,7 @@ int main(int argc, char* argv[])
 //                    csrRowPtr, csrColInd, blocksize, bsr_descr, bsrVal, bsrRowPtr, bsrColInd);
 //
 //      // pack A
-//      ToBit32Col<float><<<dim3(CEIL(blocksize), CEIL(nblocks * blocksize)), 32>>>(bsrVal, tA, blocksize, nblocks * blocksize); // sparse matrix
+//      ToBit32Col<float><<<dim3(1, nblocks), 32>>>(bsrVal, tA, blocksize, nblocks * blocksize); // sparse matrix
 //
 //    }
 
@@ -201,7 +223,7 @@ int main(int argc, char* argv[])
         else B[i] = (x > 0.5) ? 1 : 0;
     }
     printf("initialize a vector with size %d x 1\n", (nblockrows * blocksize));
-//    printf("orivec: \n"); printvec(B, (nblockrows * blocksize));
+    //printf("orivec: \n"); printvec(B, (nblockrows * blocksize));
 
     // copy to device
 	float *fB;
@@ -211,7 +233,7 @@ int main(int argc, char* argv[])
     // pack B
     unsigned *tB;
     cudaMalloc(&tB, nblockrows * 1 * sizeof(unsigned)); // (nblockrows * blocksize) / 32 = nblockrows
-    ToBit32Row<float><<<dim3(CEIL(nblockrows * blocksize), CEIL(1)), 32>>>(fB, tB, nblockrows * blocksize, 1); // dense vector
+    ToBit32Row<float><<<dim3(nblockrows, 1), 32>>>(fB, tB, nblockrows * blocksize, 1); // dense vector
 
 	// time measurement setup -----------------------------------
 	cudaEvent_t start, stop;
@@ -366,4 +388,277 @@ int main(int argc, char* argv[])
 //    free(result_cusparsebsrspmvfloat);
 //    free(result_cusparsecsrspmvfloat);
 
+}
+
+int main64(int argc, char* argv[])
+{
+
+    cudaSetDevice(0);
+    if (argc < 2)
+    {
+        printf("./exe [xxx.mtx]\n");
+        exit(1);
+    }
+
+    // matrix storage -----------------------------------
+    // read sparse matrix from file and store as csr format
+    // matrix metadata
+    char *filename = argv[1]; // e.g. "G43.mtx"
+    printf("input sparse matrix: %s\n", filename);
+
+    int nrows, ncols, nnz, isSymmetric;
+    mmio_info<float>(&nrows, &ncols, &nnz, &isSymmetric, filename);
+    printf("nrows: %d, ncols: %d, nnz: %d, isSymmetric: ", nrows, ncols, nnz); printf(isSymmetric?"true\n":"false\n");
+
+    // matrix csr in host
+    int* h_csrRowPtr, *h_csrColInd;
+    float* h_csrVal;
+    h_csrRowPtr = (int*) malloc(sizeof(int) * (nrows+1));
+    h_csrColInd = (int*) malloc(sizeof(int) * nnz);
+    h_csrVal = (float*) malloc(sizeof(float) * nnz);
+    mmio_data<float>(h_csrRowPtr, h_csrColInd, h_csrVal, filename);
+
+    // copy csr to device
+    int* csrRowPtr, *csrColInd;
+    float* csrVal;
+    cudaMalloc(&csrRowPtr, sizeof(int) * (nrows+1));
+    cudaMalloc(&csrColInd, sizeof(int) * nnz);
+    cudaMalloc(&csrVal, sizeof(float) * nnz);
+    cudaMemcpy(csrRowPtr, h_csrRowPtr, sizeof(int) * (nrows+1), cudaMemcpyHostToDevice);
+    cudaMemcpy(csrColInd, h_csrColInd, sizeof(int) * nnz, cudaMemcpyHostToDevice);
+    cudaMemcpy(csrVal, h_csrVal, sizeof(float) * nnz, cudaMemcpyHostToDevice);
+
+	// transform from csr to bsr using cuSPARSE
+	int* bsrRowPtr, *bsrColInd;
+	float* bsrVal;
+	int blocksize = 64;
+	int mb = (nrows + blocksize-1)/blocksize;
+    int nb = (ncols + blocksize-1)/blocksize;
+    int nblockrows = mb;
+
+	// cuSPARSE API metadata setup
+    cusparseMatDescr_t csr_descr = 0;
+    cusparseCreateMatDescr(&csr_descr);
+    cusparseSetMatType(csr_descr,CUSPARSE_MATRIX_TYPE_GENERAL);
+    cusparseSetMatIndexBase(csr_descr,CUSPARSE_INDEX_BASE_ZERO);
+    cusparseMatDescr_t bsr_descr = 0;
+    cusparseCreateMatDescr(&bsr_descr);
+    cusparseSetMatType(bsr_descr, CUSPARSE_MATRIX_TYPE_GENERAL);
+    cusparseSetMatIndexBase(bsr_descr, CUSPARSE_INDEX_BASE_ZERO);
+    cudaStream_t streamId = 0;
+    cusparseHandle_t handle = 0;
+    cusparseCreate(&handle);
+    cusparseSetStream(handle, streamId);
+    cusparseDirection_t dirA = CUSPARSE_DIRECTION_COLUMN;
+
+    // csr2bsr in column-major order, estimate first
+    int nblocks;
+
+    cudaMalloc((void**)&bsrRowPtr, sizeof(int) *(nblockrows+1));
+    cusparseXcsr2bsrNnz(handle, dirA, nrows, ncols, csr_descr,
+                        csrRowPtr, csrColInd, blocksize, bsr_descr, bsrRowPtr, &nblocks);
+    cudaMalloc((void**)&bsrColInd, sizeof(int)*nblocks);
+    printf("blocksize: %d, nblockrows: %d, nblocks: %d\n", blocksize, nblockrows, nblocks);
+
+    // packed matrix
+    ullong* tA;
+    cudaMalloc((void**)&tA, nblocks * blocksize * sizeof(ullong));
+
+//    if (nblocks > 100000) { // Large Matrices: batch csr2bsr & pack A at the same time
+        csr2bsr_batch_64(h_csrRowPtr, h_csrColInd, nrows, ncols, nnz,
+                      bsrRowPtr, bsrColInd, tA, blocksize, nblockrows, nblocks);
+//        printf("packmat: \n"); printpackmat<<<1,1>>>(tA, nblocks, blocksize);
+
+//    } else { // Small Matrices: csr2bsr & pack A
+//       cudaMalloc((void**)&bsrVal, sizeof(float)*(blocksize*blocksize)*nblocks);
+//       cusparseScsr2bsr(handle, dirA, nrows, ncols, csr_descr, csrVal,
+//                    csrRowPtr, csrColInd, blocksize, bsr_descr, bsrVal, bsrRowPtr, bsrColInd);
+//
+//      // pack A
+//      ToBit32Col<float><<<dim3(2, nblocks, 32>>>(bsrVal, tA, blocksize, nblocks * blocksize); // sparse matrix
+//
+//    }
+
+    // input vector and result vector storage -----------------------------------
+    // generate random vector
+    srand(time(0));
+	float *B = (float*)malloc((nblockrows * blocksize) * 1 * sizeof(float));
+	for (int i = 0; i < (nblockrows * blocksize) * 1; i ++)
+    {
+        float x = (float)rand() / RAND_MAX;
+        if (i >= ncols) B[i] = 0;
+        else B[i] = (x > 0.5) ? 1 : 0;
+    }
+    printf("initialize a vector with size %d x 1\n", (nblockrows * blocksize));
+//    printf("origvec: \n"); printvec(B, (nblockrows * blocksize));
+
+    // copy to device
+	float *fB;
+	cudaMalloc(&fB, (nblockrows * blocksize) * 1 * sizeof(float));
+	cudaMemcpy(fB, B, (nblockrows * blocksize) * 1 * sizeof(float), cudaMemcpyHostToDevice);
+
+    // pack B
+    ullong *tB;
+    cudaMalloc(&tB, nblockrows * 1 * sizeof(ullong)); // (nblockrows * blocksize) / 64 = nblockrows
+    ToBit64Row<float><<<dim3(nblockrows, 1), 32>>>(fB, tB, nblockrows * blocksize, 1); // dense vector
+//    printf("packvec: \n"); printpackvec<<<1,1>>>(tB, nblockrows);
+
+	// time measurement setup -----------------------------------
+	cudaEvent_t start, stop;
+	float milliseconds = 0;
+
+    // ============================================= BSTC-64 bsr bmv
+    // init C
+    float *fC;
+    cudaMalloc(&fC, (nblockrows * blocksize) * 1 * sizeof(float));
+    cudaMemset(fC, 0, (nblockrows * blocksize) * 1 * sizeof(float));
+
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    // get grid dim
+    double nbr = cbrt((double)nblockrows);
+    int blockdim = (int)ceil(nbr);
+    printf("cbrt(nblockrows) = %d\n", blockdim);
+    dim3 grid(blockdim, blockdim, blockdim);
+
+    // ------
+    cudaEventRecord(start);
+    for (int i=0; i<TEST_TIMES; i++) { // follow warp consolidation model (32 threads per block)
+
+        bmv64_sparse<int, float><<<grid, 32>>>(tA, tB, fC, blocksize, nblocks, 1, bsrRowPtr, bsrColInd, nblockrows, nblocks);
+    }
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds,start,stop);
+    double bmv64_time = (milliseconds*1e3)/double(TEST_TIMES);
+
+    cudaFree(tA);
+    cudaFree(tB);
+    // ------
+
+    float* result_bsrbmv64 = (float*)malloc(nrows * 1 * sizeof(float)); // don't care padding result
+    cudaMemcpy(result_bsrbmv64, fC, nrows * 1 * sizeof(float), cudaMemcpyDeviceToHost);
+    //printf("result_bsrbmv64: \n"); printvec(result_bsrbmv64, ncols);
+    printf("nnz in vec: %d\n", countnnzinvec(result_bsrbmv64, nrows));
+
+    // ============================================= cuSPARSE bsr spmv-float 64
+    // y = α ∗ op ( A ) ∗ x + β ∗ y
+//    // allocate vector x and vector y large enough for bsrmv
+//    float *x, *y;
+//    cudaMalloc((void**)&x, sizeof(float)*(nblockrows * blocksize));
+//    cudaMemcpy(x, fB, sizeof(float)*ncols, cudaMemcpyHostToDevice);  // [ncols] to [nb * blocksize] (paddings) is not moved
+//    cudaMalloc((void**)&y, sizeof(float)*(nblockrows * blocksize));
+//    cudaMemset(y, 0, sizeof(float)*ncols);
+//
+//    // perform bsrmv
+//    float alpha = 1.0, beta = 0.0;
+//    cusparseOperation_t transA = CUSPARSE_OPERATION_NON_TRANSPOSE;
+//
+//    cudaEventCreate(&start);
+//    cudaEventCreate(&stop);
+//    // ------
+//    cudaEventRecord(start);
+//    for (int i=0; i<TEST_TIMES; i++) {
+//        cusparseSbsrmv(handle, dirA, transA, mb, nb, nblocks, &alpha,
+//                    bsr_descr, bsrVal, bsrRowPtr, bsrColInd, blocksize, x, &beta, y);
+//    }
+//    cudaEventRecord(stop);
+//    cudaEventSynchronize(stop);
+//
+//    milliseconds = 0;
+//    cudaEventElapsedTime(&milliseconds,start,stop);
+//    double cusparsebsrspmvfloat_time = (milliseconds*1e3)/double(TEST_TIMES);
+//    // ------
+//
+//    float* result_cusparsebsrspmvfloat = (float*)malloc(ncols * 1 * sizeof(float));
+//    cudaMemcpy(result_cusparsebsrspmvfloat, y, ncols * 1 * sizeof(float), cudaMemcpyDeviceToHost);
+//    //printf("baselinevec: \n"); printresvec(result_cusparsebsrspmvfloat, nrows);
+
+    // ============================================= cuSPARSE csr spmv-float
+//    cusparseHandle_t handle_csr;
+//    cusparseMatDescr_t mat_A;
+//    cusparseStatus_t cusparse_status;
+//
+//    cusparseCreate(&handle_csr);
+//    cusparseCreateMatDescr(&mat_A);
+//    cusparseSetMatType(mat_A, CUSPARSE_MATRIX_TYPE_GENERAL);
+//    cusparseSetMatIndexBase(mat_A, CUSPARSE_INDEX_BASE_ZERO);
+//
+//    // create dense vector
+//    float *dX, *dY;
+//    cudaMalloc((void**)&dX, sizeof(float)*(nblockrows * blocksize));
+//    cudaMemcpy(dX, fB, sizeof(float)*ncols, cudaMemcpyHostToDevice);  // [ncols] to [nb * blocksize] (paddings) is not moved
+//    cudaMalloc((void**)&dY, sizeof(float)*(nblockrows * blocksize));
+//    cudaMemset(dY, 0, sizeof(float)*ncols);
+//
+//    cudaEventCreate(&start);
+//    cudaEventCreate(&stop);
+//
+//    // ------
+//    cudaEventRecord(start);
+//    for (int i=0; i<TEST_TIMES; i++) {
+//        cusparseScsrmv(handle_csr, CUSPARSE_OPERATION_NON_TRANSPOSE, nrows, ncols, nnz,
+//                    &alpha, mat_A, csrVal, csrRowPtr, csrColInd, dX, &beta, dY);
+//    }
+//
+//    cudaEventRecord(stop);
+//    cudaEventSynchronize(stop);
+//
+//    milliseconds = 0;
+//    cudaEventElapsedTime(&milliseconds,start,stop);
+//    double cusparsecsrspmvfloat_time = (milliseconds*1e3)/double(TEST_TIMES);
+//    // ------
+//
+//    float* result_cusparsecsrspmvfloat = (float*)malloc(ncols * 1 * sizeof(float));
+//    cudaMemcpy(result_cusparsecsrspmvfloat, dY, ncols * 1 * sizeof(float), cudaMemcpyDeviceToHost);
+//    //printf("csrspmvvec: \n"); printresvec(result_cusparsecsrspmvfloat, nrows);
+
+
+    //============================================= CHECK RESULT
+    //printf("CuSPARSE BSR SpMV-float (baseline) success: %d\n", check_result(result_cusparsebsrspmvfloat, result_cusparsebsrspmvfloat, nrows));
+    //printf("BSR BMV-64 success: %d\n", check_result(result_bsrbmv64, result_cusparsebsrspmvfloat, nrows));
+
+    printf("BSR BMV-64: %.3lf\n", bmv64_time);
+//    printf("CuSPARSE BSR SpMV-float: %.3lf\n", cusparsebsrspmvfloat_time);
+//    printf("CuSPARSE CSR SpMV-float: %.3lf\n", cusparsecsrspmvfloat_time);
+
+    // free descr and handle memory
+    cusparseDestroyMatDescr(csr_descr);
+    cusparseDestroyMatDescr(bsr_descr);
+    cusparseDestroy(handle);
+
+    // free cusparse bsr spmv
+//    cudaFree(x);
+//    cudaFree(y);
+//
+//    // free cusparse csr spmv
+//    cusparseDestroyMatDescr(mat_A);
+//    cusparseDestroy(handle_csr);
+//    cudaFree(dX);
+//    cudaFree(dY);
+
+    // free mem
+    free(h_csrRowPtr);
+    free(h_csrColInd);
+    free(h_csrVal);
+
+    cudaFree(csrRowPtr);
+    cudaFree(csrColInd);
+    cudaFree(csrVal);
+
+    cudaFree(fB);
+    cudaFree(fC);
+
+    // free all results
+    free(result_bsrbmv64);
+//    free(result_cusparsebsrspmvfloat);
+//    free(result_cusparsecsrspmvfloat);
+}
+
+int main(int argc, char* argv[])
+{
+    main32(argc, argv);
+    //main64(argc, argv);
 }
--- bsrbmv/bsrbmv.cu
+++ bsrbmv/bsrbmv.cu
@@ -2,11 +2,6 @@
 #include <assert.h>
 #include <sys/time.h>
 
-#define BITWIDTH 32
-#define LOG_BITWIDTH 5
-#define CEIL(X) (((X)+BITWIDTH-1)>>LOG_BITWIDTH)
-#define FEIL(X) ((((X)+BITWIDTH-1)>>LOG_BITWIDTH)<<LOG_BITWIDTH)
-
 typedef unsigned long long ullong;
 
 // A faster way to obtain lane id in a warp
@@ -39,8 +34,7 @@ void bin(unsigned n)
 
 // col-major packing bit 32
 template <typename T>
-__global__ void ToBit32Col(const T* __restrict__ A, unsigned* B,
-        const int A_height, const int A_width) // blocksize, nblocks * blocksize
+__global__ void ToBit32Col(const T* __restrict__ A, unsigned* B, const int A_height, const int A_width) // blocksize, nblocks * blocksize
 {
     GET_LANEID;
     const unsigned by = blockIdx.y; // nblocks
@@ -58,8 +52,7 @@ __global__ void ToBit32Col(const T* __restrict__ A, unsigned* B,
 
 // row-major packing bit 32
 template <typename T>
-__global__ void ToBit32Row(const T* __restrict__ A, unsigned* B,
-        const int A_height, const int A_width) // no get lane?
+__global__ void ToBit32Row(const T* __restrict__ A, unsigned* B, const int A_height, const int A_width)
 {
     const unsigned bx = blockIdx.x; // blockrows
     const unsigned by = blockIdx.y; // 1
@@ -73,162 +66,44 @@ __global__ void ToBit32Row(const T* __restrict__ A, unsigned* B,
     B[bx] = Bval;
 }
 
-//// col-major packing bit 64
-//template <typename T>
-//__global__ void ToBit64Col(const T* __restrict__ A, ullong* B, const int A_height, const int A_width)
-//{
-//    GET_LANEID;
-//    const unsigned by = blockIdx.y;
-//    const unsigned bx = blockIdx.x;
-//    ullong Bval;
-//#pragma unroll
-//    for (int i=0; i<32; i++)
-//    {
-//        T f0 = A[(bx*32+i)*A_width+by*64+laneid];
-//        T f1 = A[(bx*32+i)*A_width+by*64+32+laneid];
-//        unsigned r0 = __ballot(f0>0);
-//        unsigned r1 = __ballot(f1>0);
-//        ullong l0;
-//        asm volatile("mov.b64 %0, {%1,%2};":"=l"(l0):"r"(r0),"r"(r1)); //lo,hi
-//        if (laneid == i) Bval = __brevll(l0);
-//    }
-//    B[by*A_height+bx*32+laneid] = Bval;
-//}
-//
-//// row-major packing bit 64
-//template <typename T>
-//__global__ void ToBit64Row(const T* __restrict__  A, ullong* B,
-//        const int A_height, const int A_width)
-//{
-//    GET_LANEID;
-//    const unsigned bx = blockIdx.x;
-//    const unsigned by = blockIdx.y;
-//    ullong Bval = 0;
-//#pragma unroll
-//    for (int i=0; i<64; i++)
-//    {
-//        T f0 = A[(bx*64+i)*A_width+by*32+laneid];
-//        Bval = (Bval<<1) | (f0>0?1:0);
-//    }
-//    B[bx*A_width+by*32+laneid] = Bval;
-//}
+// col-major packing bit 64
+template <typename T>
+__global__ void ToBit64Col(const T* __restrict__ A, ullong* B, const int A_height, const int A_width)
+{
+    GET_LANEID;
+    const unsigned by = blockIdx.y; //nblocks
+    const unsigned bx = blockIdx.x; // 2 <- set this
+    ullong Bval;
+#pragma unroll
+    for (int i=0; i<32; i++)
+    {
+        T f0 = A[by*64*64+bx*64*32+i*64+laneid]; //
+        T f1 = A[by*64*64+bx*64*32+i*64+32+laneid]; //
+        unsigned r0 = __ballot(f0>0);
+        unsigned r1 = __ballot(f1>0);
+        ullong l0;
+        asm volatile("mov.b64 %0, {%1,%2};":"=l"(l0):"r"(r0),"r"(r1)); //lo,hi
+        if (laneid == i) Bval = __brevll(l0);
+    }
+    B[by*64+bx*32+laneid] = Bval;
+}
 
-//// the row-by-row model
-//// dense bmm
-//template <typename T>
-//__device__ __inline__ void bmm32_dense(MulAParam* p)
-//{
-//    GET_LANEID;
-//
-//    unsigned A_height = FEIL(p->weight_height);
-//    unsigned A_width = CEIL(p->weight_width);
-//    unsigned B_width = FEIL(p->input_width);
-//
-//    // load
-//    const unsigned* Asub = &(p->input_gpu[blockIdx.x*32]);
-//    const unsigned* Bsub = &(p->weight_gpu[blockIdx.y*32]);
-//    T* Csub = &(p->output_gpu[blockIdx.x*B_width*32+blockIdx.y*32]);
-//    register unsigned Cm[32] = {0}; // allocate 32 register for each lane
-//
-//    // compute
-//    for (int i=0; i<A_width; i++) {
-//        unsigned r0 = Asub[i*A_height+laneid];
-//        unsigned r1 = Bsub[i*B_width+laneid];
-//
-//        #pragma unroll
-//        for (int j=0; j<32; j++)
-//        {
-//            unsigned r2 = __shfl(r1, j); // broadcast from lane-j
-//            Cm[j] += __popc(r0 & r2);
-//        }
-//    }
-//
-//    // store
-//    for (int i=0; i<32; i++)
-//        Csub[laneid*B_width+i] = (T)(Cm[i]>0?1:0);
-//
-//    // more efficient store
-////    for (int i=0; i<32; i+=4)
-////        store128((void*)&Csub[laneid*B_width+i],
-////                    (T)(Cm[i+0]>0?1:0), (T)(Cm[i+1]>0?1:0),
-////                    (T)(Cm[i+2]>0?1:0), (T)(Cm[i+3]>0?1:0));
-//
-//}
-//
-//// dense bmv
-//template <typename T>
-//__device__ __inline__ void bmv32_dense(MulAParam* p)
-//{
-//    GET_LANEID;
-//
-//    unsigned A_height = FEIL(p->weight_height);
-//    unsigned A_width = CEIL(p->weight_width);
-//    unsigned B_width = FEIL(p->input_width);
-//
-//    // load
-//    const unsigned* Asub = &(p->input_gpu[blockIdx.x*32]);
-//    const unsigned* Bsub = &(p->weight_gpu[0]);
-//    T* Csub = &(p->output_gpu[blockIdx.x*B_width*32+blockIdx.y*32]);
-//    register unsigned Cm[1] = {0}; // allocate 1 register
-//
-//    // compute
-//    for (int i=0; i<A_width; i++) {
-//        unsigned r0 = Asub[i*A_height+laneid];
-//        unsigned r1 = Bsub[i*B_width]; // only the lane-0 is required
-//
-//        Cm[0] += __popc(r0 & r1);
-//    }
-//
-//    // store
-//    Csub[laneid*B_width] = (T)(Cm[0]>0?1:0);
-//}
-//
-
-//// bsr bmm
-//template <typename T>
-//__device__ __inline__ void bmm32_sparse(MulAParam* p) { // A (bsr matrix) * B (dense matrix) = C (dense matrix)
-//
-//    if (blockIdx.x < p->bsr_nblockrow+1) {
-//        GET_LANEID;
-//
-//        unsigned A_height = FEIL(p->weight_height); // 32 <-- not in use
-//        unsigned A_width = CEIL(p->weight_width); // 32*nblocks / 32 = nblocks <-- not in use
-//        unsigned B_width = FEIL(p->input_width); // can be 1 (feil to 32) or any possible output_width
-//
-//        // load
-//        unsigned row_start = p->bsr_rowptr_gpu[blockIdx.x]; // 0 32 64 . . . 991
-//        unsigned row_end = p->bsr_rowptr_gpu[blockIdx.x+1]; // 32 64 96 . . . 991 1022
-//        const unsigned* Asub = &(p->weight_gpu[row_start]); // block is in continuous layout
-//        const unsigned* Bsub = &(p->input_gpu[blockIdx.y*32]);
-//        T* Csub = &(p->output_gpu[blockIdx.x*B_width*32+blockIdx.y*32]);
-//        register unsigned Cm[32] = {0}; // allocate 32 register for each lane at B
-//
-//        // compute
-//        // if that row has more than 1 col block
-//        for (int i=row_start; i<row_end; i++) {
-//            unsigned r0 = Asub[i*32+laneid]; // block is in continuous layout
-//            unsigned r1 = Bsub[(p->bsr_colind_gpu[i])*B_width+laneid];
-//
-//            #pragma unroll
-//            for (int j=0; j<32; j++)
-//            {
-//                unsigned r2 = __shfl(r1, j); // broadcast from lane-j
-//                Cm[j] += __popc(r0 & r2);
-//            }
-//        }
-//
-//        // store
-////        for (int i=0; i<32; i++)
-////            Csub[laneid*B_width+i] = (T)(Cm[i]>0?1:0);
-//
-//        // more efficient store
-//        for (int i=0; i<32; i+=4)
-//            store128((void*)&Csub[laneid*B_width+i],
-//                        (T)(Cm[i+0]>0?1:0), (T)(Cm[i+1]>0?1:0),
-//                        (T)(Cm[i+2]>0?1:0), (T)(Cm[i+3]>0?1:0));
-//
-//    }
-//}
+// row-major packing bit 64
+template <typename T>
+__global__ void ToBit64Row(const T* __restrict__  A, ullong* B, const int A_height, const int A_width)
+{
+    GET_LANEID;
+    const unsigned bx = blockIdx.x;
+    const unsigned by = blockIdx.y;
+    ullong Bval = 0;
+#pragma unroll
+    for (int i=0; i<64; i++)
+    {
+        T f0 = A[bx*64+i];
+        Bval = (Bval<<1) | (f0>0);
+    }
+    B[bx] = Bval;
+}
 
 // bsr bmv32 no padding
 // A (bsr matrix) * B (vector) = C (vector)
@@ -239,11 +114,10 @@ __global__ void bmv32_sparse(const unsigned* __restrict__ A, const unsigned* __r
             const Index* __restrict__ rowptr, const Index* __restrict__ colind,
             const Index nblockrows, const Index nblocks)
 {
-    GET_LANEID;
-
     const unsigned bx = blockIdx.x * blockDim.x + blockIdx.y * blockDim.y + blockIdx.z;
-
     if (bx < nblockrows + 1) {
+        GET_LANEID;
+
         // load
         int row_start = rowptr[bx]; // 0 32 64 . . . 991
         int row_end = rowptr[bx+1]; // 32 64 96 . . . 991 1022
@@ -267,7 +141,6 @@ __global__ void bmv32_sparse(const unsigned* __restrict__ A, const unsigned* __r
     }
 }
 
-// haven't test
 // bsr bmv64 no padding
 // A (bsr matrix) * B (vector) = C (vector)
 template <typename Index, typename T>
@@ -276,31 +149,32 @@ __global__ void bmv64_sparse(const ullong* __restrict__ A, const ullong* __restr
                             const Index* __restrict__ rowptr, const Index* __restrict__ colind,
                             const Index nblockrows, const Index nblocks)
 {
-    if (blockIdx.x < nblockrows + 1) {
+    const unsigned bx = blockIdx.x * blockDim.x + blockIdx.y * blockDim.y + blockIdx.z;
+    if (bx < nblockrows + 1) {
         GET_LANEID;
 
         // load
-        unsigned row_start = rowptr[blockIdx.x]; // not correct
-        unsigned row_end = rowptr[blockIdx.x+1];
-        const ullong* Asub = &(A[row_start]);
+        unsigned row_start = rowptr[bx];
+        unsigned row_end = rowptr[bx+1];
+        const ullong* Asub = &(A[row_start*64]);
         const ullong* Bsub = &(B[0]);
-        T* Csub = &(C[blockIdx.x*B_width*64]);
-        register unsigned Cm[2] = {0};
+        T* Csub = &(C[bx*64]);
+        register unsigned Cm[1] = {0};
 
         // compute
         for (int i=row_start; i<row_end; i++) {
-            ullong a0 = Asub[i*A_height+laneid];
-            ullong a1 = Asub[i*A_height+32+laneid];
-            ullong b0 = Bsub[(colind[i])*B_width];
+            Cm[0] = 0;
+            ullong a0 = Asub[(i-row_start)*64+laneid];
+            ullong a1 = Asub[(i-row_start)*64+32+laneid];
+            ullong b0 = Bsub[colind[i]];
 
-            Cm[0] += (__popcll(a0 & b0)<<16) + __popcll(a1 & b0);
+            Cm[0] += (__popcll(a0 & b0) << 16) + __popcll(a1 & b0);
+
+            // store
+            short t0, t1;
+            asm volatile("mov.b32 {%0,%1}, %2;":"=h"(t1),"=h"(t0):"r"(Cm[0]));
+            Csub[laneid] += (T)t0;
+            Csub[laneid+32] += (T)t1;
         }
-
-        // store
-        short t0,t1,t2,t3;
-        asm volatile("mov.b32 {%0,%1}, %2;":"=h"(t1),"=h"(t0):"r"(Cm[0]));
-        asm volatile("mov.b32 {%0,%1}, %2;":"=h"(t3),"=h"(t2):"r"(Cm[1]));
-        store64(&Csub[laneid*B_width], (T)(t0>0?1:0), (T)(t2>0?1:0));
-        store64(&Csub[(laneid+32)*B_width], (T)(t1>0?1:0), (T)(t3>0?1:0));
     }
 }
